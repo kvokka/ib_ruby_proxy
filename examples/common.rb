055c2720ffd7aab79c42fa1702c88b122644265c
kvokka <kvokka@yahoo.com>
2020-10-11 07:16:15 +0100

    Add multiple observers example

--- examples/common.rb
+++ examples/common.rb
@@ -1,10 +1,27 @@
 require 'drb'
 require 'ib_ruby_proxy'
 require 'amazing_print'
+require 'date'
+
+# borrowed from https://stackoverflow.com/questions/43149326/ruby-rails-get-the-previous-friday-friday-of-last-week
+module Utils
+  module_function
+
+  def prior_friday(date = nil)
+    date ||= Date.today
+    days_before = (date.wday + 1) % 7 + 1
+    (date - days_before).strftime('%Y%m%d')
+  end
+
+  def order_id
+    @order_id = (@order_id || Time.now.to_i % 2**20) + 1
+  end
+end
 
 class Securities
   class << self
-    def emini(expiration_month = '201909')
+    def emini
+      expiration_month = Utils.prior_friday[0..-3]
       IbRubyProxy::Client::Ib::Contract.new symbol: 'ES',
                                             sec_type: 'FUT',
                                             currency: 'USD',
---
+++ examples/multiple_observers.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require_relative './common'
+
+class CallbacksObserver < IbRubyProxy::Client::IbCallbacksObserver
+  def historical_data(*args)
+    ap args
+  end
+
+  def historical_data_end(*); end
+end
+
+class RequestsObserver
+  include DRb::DRbUndumped
+
+  def self.processed
+    @processed ||= []
+  end
+
+  ProcessedRequest = Struct.new(:req_id, :callback, :time, keyword_init: true)
+
+  def update(callback, req_id = nil, *_args)
+    pq = ProcessedRequest.new(req_id: req_id, callback: callback.to_sym, time: Time.now)
+    self.class.processed << pq
+  end
+end
+
+aapl = IbRubyProxy::Client::Ib::Contract.new symbol: 'AAPL',
+                                             sec_type: 'STK',
+                                             exchange: 'NYSE'
+
+client = IbRubyProxy::Client::Client.from_drb
+client.add_ib_callbacks_observer CallbacksObserver.new
+client.add_ib_callbacks_observer RequestsObserver.new
+
+requests_count = (ARGV.first || 3).to_i
+
+requests_count.times do |i|
+  client.req_historical_data(Utils.order_id,
+                             aapl,
+                             "#{Utils.prior_friday} 15:#{i}0:00",
+                             '600 S',
+                             '5 mins',
+                             'TRADES',
+                             1,
+                             1,
+                             false,
+                             nil)
+end
+
+t = Time.now
+
+sleep(0.001) until RequestsObserver
+                   .processed
+                   .select { |o| o.callback == :historical_data_end }
+                   .size == requests_count
+
+puts "it took #{(Time.now - t).to_f}"
