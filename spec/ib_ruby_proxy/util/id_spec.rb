f517f1099bcc31a6d7f36ddfabba4f801098be83
kvokka <kvokka@yahoo.com>
2020-10-14 09:04:43 +0100

    Fix specs

--- spec/ib_ruby_proxy/util/id_spec.rb
+++ spec/ib_ruby_proxy/util/id_spec.rb
@@ -4,12 +4,13 @@ describe IbRubyProxy::Util::Id do
   include described_class
 
   describe '#call' do
-    it 'returns some integer' do
-      expect(call).to be_a Integer
+    it 'returns Concurrent::AtomicFixnum' do
+      expect(call).to be_a Concurrent::AtomicFixnum
     end
 
     it 'the next value is +1 from the previous one' do
-      prev = call
+      prev = call.value
+
       expect(call).to eq prev + 1
     end
   end
